31d158fc5f5b8d91ced7857f2ea96d996b5ff0ee
Eric Brusseau <eric@brusseau.org>
2019-03-08 15:14:58 -0800

    Added plugins example

--- examples/local-users-with-theme/deploy.sh
+++ examples/local-users-with-theme/deploy.sh
@@ -4,7 +4,7 @@ bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -o ../../deployment/operations/use-config-repo.yml \
   -o ../../deployment/operations/set-initial-config.yml \
   -o operations/credential-variables.yml \
-  -v deployment_name=jenkins
+  -v deployment_name=jenkins \
   -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
   -v repo_branch=testing \
   -v repo_path=examples/local-users-with-theme/config-repo \
--- examples/plugins/deploy.sh
+++ examples/plugins/deploy.sh
@@ -2,10 +2,10 @@
 
 bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -o ../../deployment/operations/use-config-repo.yml \
-  -o ../../deployment/operations/set-initial-config.yml \
+  -o ../../deployment/operations/install-plugins.yml \
   -o ../../deployment/operations/master-only.yml \
   -v deployment_name=jenkins \
   -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
   -v repo_branch=testing \
-  -v repo_path=examples/mixed-configuration/config-repo \
-  --var-file initial_config=initial_config.yml
+  -v repo_path=examples/plugins/config-repo \
+  --var-file install_plugins=plugins.txt
