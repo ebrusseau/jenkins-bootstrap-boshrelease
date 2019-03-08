59d0a37eba5d81b13a10b774460d6743a9688fcd
Eric Brusseau <eric@brusseau.org>
2019-03-08 08:21:10 -0800

    Fixed hardcoded values for slave username and password

--- examples/local-users-with-theme/deploy.sh
+++ examples/local-users-with-theme/deploy.sh
@@ -7,4 +7,6 @@ bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
   -v repo_branch=testing \
   -v repo_path=examples/local-users-with-theme/config-repo \
+  -v slave_username=agent \
+  -v slave_password=((jenkins_agent_password))
   --var-file initial_config=initial_config.yml
