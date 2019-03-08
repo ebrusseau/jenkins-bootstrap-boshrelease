085c57a0d0fe4d721b7d20983c65d5a4bd0647c0
Eric Brusseau <eric@brusseau.org>
2019-03-07 22:02:55 -0800

    Fixed mixed-configuration deploy script

--- examples/mixed-configuration/deploy.sh
+++ examples/mixed-configuration/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#!/bin/bash
 
-bosh int ../../deployment/jenkins.yml \
+bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -o ../../deployment/operations/use-config-repo.yml \
   -o ../../deployment/operations/set-initial-config.yml \
   -o ../../deployment/operations/master-only.yml \
@@ -9,13 +8,3 @@ bosh int ../../deployment/jenkins.yml \
   -v repo_branch=testing \
   -v repo_path=examples/mixed-configuration/config-repo \
   --var-file initial_config=initial_config.yml
-
-
-# bosh deploy -d jenkins ../../deployment/jenkins.yml \
-#   -o ../../deployment/operations/use-config-repo.yml \
-#   -o ../../deployment/operations/set-initial-config.yml \
-#   -o ../../deployment/operations/master-only.yml \
-#   -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
-#   -v repo_branch=testing \
-#   -v repo_path=examples/mixed-configuration/config-repo \
-#   --var-file initial_config=initial_config.yml
