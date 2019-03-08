0b8b959f8fa6a87d1a1961dbc120b2f55310eac6
Eric Brusseau <eric@brusseau.org>
2019-03-08 12:58:33 -0800

    Fixed deploy.sh for examples/enforced-configuration :| Added output logging for configuration stages

--- examples/enforced-configuration/deploy.sh
+++ examples/enforced-configuration/deploy.sh
@@ -4,6 +4,6 @@ bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -o ../../deployment/operations/set-initial-config.yml \
   -o ../../deployment/operations/set-enforced-config.yml \
   -o ../../deployment/operations/master-only.yml \
-  -v deployment_name=jenkins
-  --var-file initial_config=initial_config.yml
+  -v deployment_name=jenkins \
+  --var-file initial_config=initial_config.yml \
   --var-file enforced_config=enforced_config.yml
