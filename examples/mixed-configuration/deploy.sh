04681a60b785bc92b0a804a5866a6cf23a9be848
Eric Brusseau <eric@brusseau.org>
2019-03-07 22:01:55 -0800

    Added ability to default master mode, executors and labels through manifest elements to give it parity with slave configurtaion - can also be done with jenkins.config.initial, or through a repo, but this allows us to use BOSH operations files for these basic runtime parameters

---
+++ examples/mixed-configuration/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#!/bin/bash
+
+bosh int ../../deployment/jenkins.yml \
+  -o ../../deployment/operations/use-config-repo.yml \
+  -o ../../deployment/operations/set-initial-config.yml \
+  -o ../../deployment/operations/master-only.yml \
+  -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
+  -v repo_branch=testing \
+  -v repo_path=examples/mixed-configuration/config-repo \
+  --var-file initial_config=initial_config.yml
+
+
+# bosh deploy -d jenkins ../../deployment/jenkins.yml \
+#   -o ../../deployment/operations/use-config-repo.yml \
+#   -o ../../deployment/operations/set-initial-config.yml \
+#   -o ../../deployment/operations/master-only.yml \
+#   -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
+#   -v repo_branch=testing \
+#   -v repo_path=examples/mixed-configuration/config-repo \
+#   --var-file initial_config=initial_config.yml
