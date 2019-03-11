00c4b8e46324aed70e3ec95533608cd5858d692d
Eric Brusseau <eric@brusseau.org>
2019-03-11 10:52:03 -0700

    Updated local-users.. example to use install-plugins operations file for matrix-auth plugin

--- examples/local-users-with-theme/deploy.sh
+++ examples/local-users-with-theme/deploy.sh
@@ -3,9 +3,11 @@
 bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -o ../../deployment/operations/use-config-repo.yml \
   -o ../../deployment/operations/set-initial-config.yml \
+  -o ../../deployment/operations/install-plugins.yml \
   -o operations/credential-variables.yml \
   -v deployment_name=jenkins \
   -v repo_url=https://github.com/ebrusseau/jenkins-bootstrap-boshrelease.git \
   -v repo_branch=testing \
   -v repo_path=examples/local-users-with-theme/config-repo \
-  --var-file initial_config=initial_config.yml
+  --var-file initial_config=initial_config.yml \
+  --var-file install_plugins=plugins.txt
