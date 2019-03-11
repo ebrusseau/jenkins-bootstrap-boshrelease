de9a079cc653d3eac2229cf853fda917a27a33a9
Eric Brusseau <eric@brusseau.org>
2019-03-11 10:06:48 -0700

    Added credential and interpolation workaround to enforced config example

--- examples/enforced-configuration/deploy.sh
+++ examples/enforced-configuration/deploy.sh
@@ -6,4 +6,4 @@ bosh deploy -d jenkins ../../deployment/jenkins.yml \
   -o ../../deployment/operations/master-only.yml \
   -v deployment_name=jenkins \
   --var-file initial_config=initial_config.yml \
-  --var-file enforced_config=enforced_config.yml
+  --var-file enforced_config=<(credhub interpolate -f enforced_config.yml -p /bosh-lite/jenkins)
